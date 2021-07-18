0af699a8f8bd17b6be7ed257ff2709d735355899
Yumaxew <72713020+yumaxew@users.noreply.github.com>
2021-07-18 17:14:47 +0100

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,12 @@ var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function(){
     if(this.window.pageYOffset > 100) {
-        nav.classList.add('bg-dark', 'text-white', 'shadow');
+        nav.classList.add('bg-dark', 'shadow');
 
     }else{
-        nav.classList.remove('bg-dark', 'text-white', 'shadow');
+        nav.classList.remove('bg-dark', 'shadow');
 
     }
 });
 
 
-var menuIcon = document.getElementById('iconMenu');
-
-window.addEventListener('scroll', function(){
-    if(this.window.pageYOffset > 100) {
-        menuIcon.style.color = 'transparent';
-
-    }else{
-        menuIcon.style.color = '#1A1A1A';
-
-    }
-});
